843215dc198232552893c036e8800b67d0ebac46
Anton-98 <thioyeb0@gmail.com>
2023-07-21 17:47:51 +0000

    a

--- lib/components/constantes.dart
+++ lib/components/constantes.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 const cBleuFonce = Color(0xff1b61ae);
 const cBleuClair = Color(0xff0491e8);
-const cBlanc = Color(0x00110001);
+const cBlanc = Color.fromARGB(255, 235, 234, 234);
 const cNoir = Color(0xff111824);
--- lib/screens/sidebar.dart
+++ lib/screens/sidebar.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:safe_droid/components/constantes.dart';
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Parameter"),
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      decoration: const BoxDecoration(
+        color: cBlanc,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: size.width,
+            height: size.width * 0.2,
+            padding: EdgeInsets.only(
+              left: size.width * 0.1 - 20,
+              top: size.width * 0.1 - 20,
+            ),
+            decoration: const BoxDecoration(
+                // color: cBleuFonce,
+                ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.arrow_back_ios,
+                  size: 36,
+                  color: cBleuFonce,
+                ),
+                SizedBox(
+                  width: size.width * 0.2,
+                ),
+                Text(
+                  "Parametres",
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                )
+              ],
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Back"))
+        ],
       ),
     );
   }
